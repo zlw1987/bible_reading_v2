f49cf7d8828d300a1de8ed15b0d76c8af1421ce9
zlw1987 <38115755+zlw1987@users.noreply.github.com>
2026-05-13 18:10:22 -0700

    Initialize Django in GoDaddy scripts

--- run_create_admin_godaddy.py
+++ run_create_admin_godaddy.py
@@ -8,6 +8,9 @@ sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings_godaddy")
 
+import django
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 
